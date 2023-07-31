3ac37a1eba483c81e8609d23bf29a7c1de64c2a2
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-07-31 08:36:45 -0300

    Create example.rs

---
+++ example.rs
@@ -0,0 +1,128 @@
+use bdk::blockchain::noop_progress;
+use bdk::electrum_client::Client;
+use bdk::Wallet;
+use bdk::KeychainKind;
+use bdk::keys::bip39::{Language, Mnemonic, MnemonicType, Seed};
+use ldk::chain::{chaininterface::ChainWatchInterfaceUtil, transaction::OutPoint, transaction::Transaction, OutPoint, Txid};
+use ldk::chain::keysinterface::SpendableOutputDescriptor;
+use ldk::lightning::chain;
+use ldk::lightning::ln::chan_utils;
+use ldk::ln::channelmonitor::ManyChannelMonitor;
+use ldk::ln::chan_utils::ChannelPublicKeys;
+use ldk::ln::chan_utils::TxCreationKeys;
+use ldk::ln::channelmonitor::ChannelMonitor;
+use ldk::ln::msgs::{AcceptChannel, AnnouncementSignatures, ChannelReestablish, CommitmentSigned, FundingLocked, FundingSigned, RevokeAndACK, Shutdown, UpdateAddHTLC, UpdateFulfillHTLC, UpdateFailHTLC, UpdateFailMalformedHTLC, UpdateFee, Init};
+use ldk::ln::channelmonitor::ChannelMonitorUpdate;
+use ldk::ln::msgs::DecodeError;
+use ldk::ln::msgs::DecodeError::InvalidValue;
+use ldk::ln::msgs::LightningError;
+use ldk::ln::msgs::OpenChannel;
+use ldk::ln::msgs::OpenChannel2;
+use ldk::ln::msgs::OptionalField;
+use ldk::ln::msgs::HandleError;
+use ldk::ln::msgs::ErrorAction;
+use ldk::ln::msgs::ErrorMessage;
+use ldk::ln::msgs::InitFeatures;
+use ldk::ln::msgs::ChannelMessageHandler;
+use ldk::ln::msgs::RoutingMessageHandler;
+use ldk::ln::msgs::PeerHandleError;
+use ldk::ln::msgs::IGNORABLE_MSG_GLOBAL_UPGRADE1;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_CHANNEL_RANGE_QUERY;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_CHANNEL_RANGE_QUERY_CHANNEL_RANGE_LEN;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_CHANNEL_RANGE_QUERY_SHORT_CHANNEL_IDS_LEN;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_SHORT_CHANNEL_IDS_END;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_SHORT_CHANNEL_IDS_LEN;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_SHORT_CHANNEL_IDS_NUM_SHORT_IDS;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_CHANNEL_RANGE_QUERY_CHAIN_LEN;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_CHANNEL_RANGE_QUERY_QUERY_FLAGS;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_CHANNEL_RANGE_QUERY_SHORT_CHANNEL_ID_FULL_LEN;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_CHANNEL_RANGE_QUERY_FULL_LEN;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_CHANNEL_RANGE_LEN;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_CHANNEL_RANGE_NUM_SHORT_CHANS;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_CHANNEL_RANGE_QUERY_FULL_CHANNELS_LEN;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_CHANNEL_RANGE_QUERY_FULL_SHORT_CHANNEL_IDS_LEN;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_CHANNEL_RANGE_QUERY_SHORT_CHANNEL_IDS_NUM_SHORT_IDS;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_CHANNEL_RANGE_QUERY_SHORT_CHANNEL_IDS_LEN;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_CHANNEL_RANGE_QUERY_FULL_CHANNELS_CHAIN_LEN;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_CHANNEL_RANGE_QUERY_FULL_CHANNELS_FLAG;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_CHANNEL_RANGE_QUERY_FULL_SHORT_CHANNEL_IDS_CHAIN_LEN;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_CHANNEL_RANGE_QUERY_FULL_SHORT_CHANNEL_IDS_FLAG;
+use ldk::ln::msgs::ChannelRangeQuery;
+use ldk::ln::msgs::ChannelRangeQueryFlags;
+use ldk::ln::msgs::QueryChannelRange;
+use ldk::ln::msgs::ReplyChannelRange;
+use ldk::ln::msgs::ReplyShortChannelIdsEnd;
+use ldk::ln::msgs::ReplyShortChannelIdsEnds;
+use ldk::ln::msgs::ReplyShortChannelIdsQuery;
+use ldk::ln::msgs::ShortChannelIdsEnd;
+use ldk::ln::msgs::ShortChannelIdsQuery;
+use ldk::ln::msgs::ShortChannelIdsQueryWithList;
+use ldk::ln::msgs::QueryShortChannelIds;
+use ldk::ln::msgs::ReplyShortChannelIdsQueryChannelRange;
+use ldk::ln::msgs::ShortChannelIdsQueryChannelRange;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_SHORT_CHANNEL_IDS_END_SHORT_CHANNEL_IDS_NUM_SHORT_IDS;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_SHORT_CHANNEL_IDS_END_SHORT_CHANNEL_IDS_LEN;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_SHORT_CHANNEL_IDS_END_LEN;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_SHORT_CHANNEL_IDS_END_FULL_LEN;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_SHORT_CHANNEL_IDS_END_NUM_SHORT_IDS;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_SHORT_CHANNEL_IDS_END_QUERY_SHORT_CHANNEL_IDS_LEN;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_SHORT_CHANNEL_IDS_END_QUERY_SHORT_CHANNEL_IDS_CHAIN_LEN;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_SHORT_CHANNEL_IDS_END_QUERY_SHORT_CHANNEL_IDS_FLAG;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_SHORT_CHANNEL_IDS_END_SHORT_CHANNEL_ID_LEN;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_SHORT_CHANNEL_IDS_END_QUERY_SHORT_CHANNEL_IDS_NUM_SHORT_IDS;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_SHORT_CHANNEL_IDS_END_QUERY_SHORT_CHANNEL_IDS_END;
+use ldk::ln::msgs::IGNORABLE_MSG_REPLY_SHORT_CHANNEL_IDS_END_QUERY_SHORT_CHANNEL_IDS;
+
+fn main() {
+    // Setup LDK, BDK, and other dependencies
+    // ...
+    // ...
+
+    // In this example, we assume the user already has an on-chain UTXO
+
+    // User wants to lift their UTXO for a virtual UTXO (1:1 ratio)
+    let virtual_utxo = lift_to_virtual_utxo(&user_on_chain_utxo);
+
+    // Later, if the user wants to redeem the virtual UTXO for an on-chain UTXO,
+    // they can use the "unilateral_redeem" function without requiring ASP cooperation.
+    let redeemed_on_chain_utxo = unilateral_redeem(&virtual_utxo);
+
+    // Continue with the rest of your program logic...
+}
+
+// Function to lift an on-chain UTXO to a virtual UTXO
+fn lift_to_virtual_utxo(on_chain_utxo: &Transaction) -> VirtualUTXO {
+    // Your implementation here for lifting the on-chain UTXO
+    // and generating the virtual UTXO
+    // ...
+
+    // For this example, we will just assume that the virtual UTXO is created
+    // with the same value and details as the on-chain UTXO
+    VirtualUTXO {
+        value: on_chain_utxo.total_value(),
+        script_pubkey: on_chain_utxo.output[0].script_pubkey.clone(),
+        // Other necessary fields to represent a virtual UTXO
+    }
+}
+
+// Function to unilaterally redeem the virtual UTXO for an on-chain UTXO
+fn unilateral_redeem(virtual_utxo: &VirtualUTXO) -> Transaction {
+    // Your implementation here for redeeming the virtual UTXO unilaterally
+    // and generating the corresponding on-chain UTXO
+    // ...
+
+    // For this example, we will just assume that the on-chain UTXO is created
+    // with the same value and details as the virtual UTXO
+    Transaction {
+        inputs: vec![OutPoint::new(virtual_utxo.txid, 0)],
+        outputs: vec![virtual_utxo.output.clone()],
+        // Other necessary fields to represent the on-chain UTXO
+    }
+}
+
+// Definition of the VirtualUTXO struct representing a virtual UTXO
+struct VirtualUTXO {
+    value: u64,
+    script_pubkey: Script,
+    // Other fields as needed
+}
